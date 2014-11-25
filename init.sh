2cef56710f11468fb7a698f38ef0d7ce323a68a3
John Swanson <john.d.swanson@gmail.com>
2014-11-24 16:04:49 -0800

    add init script

---
+++ init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+# $DIR is the full directory name this script is stored in (i.e. the repo)
+
+# install salt
+wget -O - https://bootstrap.saltstack.com | sh
+
+# move configuration into place
+ln -s $DIR/etc/salt /etc/salt/minion
+
+ln -s $DIR/srv/salt /srv/salt
+
